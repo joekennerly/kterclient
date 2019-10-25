76dbdd0de17a895fda3a41dfef2db6b42c03be2f
joekennerly <joseph.kennerly@gmail.com>
2019-10-25 11:04:46 -0500

    added actual form inputs

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,5 +1,11 @@
 import React from "react"
 
-const Login = () => <h1>Login form</h1>
+const Login = () =>
+    <>
+        <h1>login form</h1>
+        <input type="text" placeholder="username"/>
+        <input type="password" placeholder="password"/>
+        <button>Login</button>
+    </>
 
 export default Login
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,5 +1,18 @@
 import React from "react"
 
-const Register = () => <h1>register form</h1>
+const Register = () =>
+    <>
+        <h1>register form</h1>
+        <input type="text" placeholder="username"/>
+        <input type="password" placeholder="password"/>
+        <input type="text" placeholder="first name"/>
+        <input type="text" placeholder="last name"/>
+        <input type="text" placeholder="email"/>
+        <input type="text" placeholder="company_name"/>
+        <input type="text" placeholder="phone number"/>
+        <input type="text" placeholder="city"/>
+        <input type="text" placeholder="bio" />
+        <button>Register</button>
+    </>
 
 export default Register
