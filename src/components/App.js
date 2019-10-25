d03f89ebda5c759cbd37a103b26e0682e8fbff4d
joekennerly <joseph.kennerly@gmail.com>
2019-10-25 10:14:09 -0500

    add application views

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./App.css"
+import ApplicationViews from "./ApplicationViews"
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <ApplicationViews />
+            </header>
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Route, Redirect } from "react-router-dom"
+import { withRouter } from "react-router"
+
+
+const ApplicationViews = () =>
+    <>
+        hey
+    </>
+
+export default withRouter(ApplicationViews)
