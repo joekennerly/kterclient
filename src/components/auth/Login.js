85f7610a707598c77bef3c43deea106298bde8a6
joekennerly <joseph.kennerly@gmail.com>
2019-10-25 11:54:01 -0500

    can register new vendor

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,11 +1,13 @@
 import React from "react"
 
+const handleLogin = () => console.log("login clicked")
+
 const Login = () =>
     <>
         <h1>login form</h1>
         <input type="text" placeholder="username"/>
         <input type="password" placeholder="password"/>
-        <button>Login</button>
+        <button onClick={handleLogin}>Login</button>
     </>
 
 export default Login
