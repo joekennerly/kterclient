a4bf39d6450d51560e483004c1941055eae467f8
joekennerly <joseph.kennerly@gmail.com>
2019-11-04 14:03:36 -0600

    navbar work

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,14 +1,14 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React, { useState } from "react"
 import useSimpleAuth from "../../hooks/ui/useSimpleAuth"
-import "./Navbar.css"
 import { makeStyles } from "@material-ui/core/styles"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import Typography from "@material-ui/core/Typography"
 import Button from "@material-ui/core/Button"
 import IconButton from "@material-ui/core/IconButton"
-// import MenuIcon from "@material-ui/icons/Menu"
+import MenuIcon from "@material-ui/icons/Menu"
+import Menu from "@material-ui/core/Menu"
+import MenuItem from "@material-ui/core/MenuItem"
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -22,7 +22,15 @@ const useStyles = makeStyles(theme => ({
     }
 }))
 
-const Navbar = () => {
+const Navbar = props => {
+    const [anchorEl, setAnchorEl] = useState(null)
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget)
+    }
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
+
     const classes = useStyles()
     const { isAuthenticated, logout } = useSimpleAuth()
     return (
@@ -30,49 +38,76 @@ const Navbar = () => {
             <div className={classes.root}>
                 <AppBar position="static">
                     <Toolbar>
-                        <IconButton
-                            edge="start"
-                            className={classes.menuButton}
-                            color="inherit"
-                            aria-label="menu"
-                        >
-                            <MenuIcon />
-                        </IconButton>
+                        {isAuthenticated() ? (
+                            <IconButton
+                                edge="start"
+                                className={classes.menuButton}
+                                color="inherit"
+                                aria-label="menu"
+                            >
+                                <MenuIcon
+                                    aria-controls="simple-menu"
+                                    aria-haspopup="true"
+                                    onClick={handleClick}
+                                />
+                                <Menu
+                                    id="simple-menu"
+                                    anchorEl={anchorEl}
+                                    keepMounted
+                                    open={Boolean(anchorEl)}
+                                    onClose={handleClose}
+                                >
+                                    <MenuItem
+                                        onClick={() => {
+                                            props.history.push("/")
+                                            handleClose()
+                                        }}
+                                    >
+                                        Home
+                                </MenuItem>
+                                    <MenuItem
+                                        onClick={() => {
+                                            props.history.push("/profile")
+                                            handleClose()
+                                        }}
+                                    >
+                                        Profile
+                                </MenuItem>
+                                </Menu>
+                            </IconButton>
+                        ) : (<div/>)}
                         <Typography variant="h6" className={classes.title}>
-                            News
+                            Kter
                         </Typography>
-                        <Button color="inherit">Login</Button>
+                        {isAuthenticated() ? (
+                            <Button
+                                color="inherit"
+                                    onClick={() => {
+                                        logout()
+                                        props.history.push("/")
+                                    }}
+                            >
+                                Logout
+                            </Button>
+                        ) : (
+                            <>
+                            <Button
+                                color="inherit"
+                                onClick={() => props.history.push("/login")}
+                            >
+                                Login
+                            </Button>
+                            <Button
+                                color="inherit"
+                                onClick={() => props.history.push("/register")}
+                            >
+                                Register
+                            </Button>
+                            </>
+                        )}
                     </Toolbar>
                 </AppBar>
             </div>
-            <Link className="nav-link" to="/">
-                Home
-            </Link>
-            {isAuthenticated() ? (
-                <>
-                    <Link className="nav-link" to="/profile">
-                        Profile
-                    </Link>
-                    <Link
-                        to="/"
-                        className="nav-link fakeLink"
-                        onClick={() => {
-                            logout()
-                        }}
-                    >
-                        Logout
-                    </Link>
-                </>
-            ) : (
-                <>
-                    <Link className="nav-link" to="/login">
-                        Login
-                    </Link>
-                    <Link className="nav-link" to="/register">
-                        Register
-                    </Link>
-                </>
-            )}
         </>
     )
 }
