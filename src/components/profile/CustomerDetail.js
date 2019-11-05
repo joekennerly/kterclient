337774fd460f86f7a7d9df5386f4cb82017dffba
joekennerly <joseph.kennerly@gmail.com>
2019-11-05 14:48:55 -0600

    refactored event form; event may not start before today; end may not occur before start

--- src/components/profile/CustomerDetail.js
+++ src/components/profile/CustomerDetail.js
@@ -7,10 +7,10 @@ import EventForm from "./EventForm"
 import Typography from "@material-ui/core/Typography"
 import Button from "@material-ui/core/Button"
 import Dialog from "@material-ui/core/Dialog"
-import DialogActions from "@material-ui/core/DialogActions"
-import DialogContent from "@material-ui/core/DialogContent"
-import DialogContentText from "@material-ui/core/DialogContentText"
-import DialogTitle from "@material-ui/core/DialogTitle"
+// import DialogActions from "@material-ui/core/DialogActions"
+// import DialogContent from "@material-ui/core/DialogContent"
+// import DialogContentText from "@material-ui/core/DialogContentText"
+// import DialogTitle from "@material-ui/core/DialogTitle"
 
 const CustomerDetail = props => {
   const [customer, setCustomer] = useState([])
@@ -156,26 +156,10 @@ const CustomerDetail = props => {
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">
-          {"Use Google's location service?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending
-            anonymous location data to Google, even when no apps are running.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseEv} color="primary">
-            Disagree
-          </Button>
-          <Button onClick={handleCloseEv} color="primary" autoFocus>
-            Agree
-          </Button>
-        </DialogActions>
         <EventForm
           customerId={customer.id}
           getOrders={() => getOrders(customer.id)}
+          handleCloseEv={handleCloseEv}
         />
       </Dialog>
       <EventList orders={orders} customer={customer} />
