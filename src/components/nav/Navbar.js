ca6850287685541a43aafa479d29ea0e4489e57a
joekennerly <joseph.kennerly@gmail.com>
2019-11-05 17:25:04 -0600

    fixed nav bar issue

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -11,113 +11,111 @@ import Menu from "@material-ui/core/Menu"
 import MenuItem from "@material-ui/core/MenuItem"
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1
-    },
-    menuButton: {
-        marginRight: theme.spacing(2)
-    },
-    title: {
-        flexGrow: 1
-    }
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
 }))
 
 const Navbar = props => {
-    const { user } = props
-    const [anchorEl, setAnchorEl] = useState(null)
-    const handleClick = event => {
-        setAnchorEl(event.currentTarget)
-    }
-    const handleClose = () => {
-        setAnchorEl(null)
-    }
+  const { user } = props
+  const [anchorEl, setAnchorEl] = useState(null)
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
 
-    const classes = useStyles()
-    const { isAuthenticated, logout } = useSimpleAuth()
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    {isAuthenticated() ? (
-                        <IconButton
-                            edge="start"
-                            className={classes.menuButton}
-                            color="inherit"
-                            aria-label="menu"
-                            >
-                            <MenuIcon
-                                aria-controls="simple-menu"
-                                aria-haspopup="true"
-                                onClick={handleClick}
-                            />
-                            <Menu
-                                id="simple-menu"
-                                anchorEl={anchorEl}
-                                keepMounted
-                                open={Boolean(anchorEl)}
-                                onClose={handleClose}
-                            >
-                                <MenuItem
-                                    onClick={() => {
-                                        props.history.push("/")
-                                        handleClose()
-                                    }}
-                                >
-                                    Home
-                                </MenuItem>
-                                <MenuItem
-                                    onClick={() => {
-                                        props.history.push("/profile")
-                                        handleClose()
-                                    }}
-                                >
-                                    Profile
-                                </MenuItem>
-                            </Menu>
-                        </IconButton>
-                    ) : (
-                        <div />
-                    )}
-                    <Typography variant="h6" className={classes.title}>
-                        K TER
-                    </Typography>
-                    {isAuthenticated() ? (
-                        <>
-                            {user.map(vendor => (
-                                <Typography key={vendor.id} id="profile">
-                                    Signed in as {vendor.user.first_name}{" "}
-                                    {vendor.user.last_name}
-                                </Typography>
-                            ))}
-                            <Button
-                                color="inherit"
-                                onClick={() => {
-                                    logout()
-                                    props.history.push("/")
-                                }}
-                            >
-                                Logout
-                            </Button>
-                        </>
-                    ) : (
-                        <>
-                            <Button
-                                color="inherit"
-                                onClick={() => props.history.push("/login")}
-                            >
-                                Login
-                            </Button>
-                            <Button
-                                color="inherit"
-                                onClick={() => props.history.push("/register")}
-                            >
-                                Register
-                            </Button>
-                        </>
-                    )}
-                </Toolbar>
-            </AppBar>
-        </div>
-    )
+  const classes = useStyles()
+  const { isAuthenticated, logout } = useSimpleAuth()
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          {isAuthenticated() ? (
+            <>
+              <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="menu"
+                onClick={handleClick}
+              >
+                <MenuIcon aria-controls="simple-menu" aria-haspopup="true" />
+              </IconButton>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem
+                  onClick={() => {
+                    props.history.push("/")
+                    handleClose()
+                  }}
+                >
+                  Home
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    props.history.push("/profile")
+                    handleClose()
+                  }}
+                >
+                  Profile
+                </MenuItem>
+              </Menu>
+            </>
+          ) : (
+            <div />
+          )}
+          <Typography variant="h6" className={classes.title}>
+            K TER
+          </Typography>
+          {isAuthenticated() ? (
+            <>
+              {user.map(vendor => (
+                <Typography key={vendor.id} id="profile">
+                  Signed in as {vendor.user.first_name} {vendor.user.last_name}
+                </Typography>
+              ))}
+              <Button
+                color="inherit"
+                onClick={() => {
+                  logout()
+                  props.history.push("/")
+                }}
+              >
+                Logout
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button
+                color="inherit"
+                onClick={() => props.history.push("/login")}
+              >
+                Login
+              </Button>
+              <Button
+                color="inherit"
+                onClick={() => props.history.push("/register")}
+              >
+                Register
+              </Button>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
 }
 export default Navbar
